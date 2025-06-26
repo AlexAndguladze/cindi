387d1f5d6a7055ddd4bb4d8a32c10aa808d917a6
FASTPAY\a.andghuladze <andghuladze.alex@gmail.com>
2025-06-26 16:26:10 +0400

    improve login page design

--- website/cindi/src/App.jsx
+++ website/cindi/src/App.jsx
@@ -6,17 +6,18 @@ import Header from "./components/Header/Header.jsx"
 import Footer from "./components/Footer/Footer.jsx"
 
 import {
-  BrowserRouter,
-  Outlet,
-  Route,
- } from "react-router-dom";
+    BrowserRouter,
+    Outlet,
+    Route, useLocation,
+} from "react-router-dom";
 
 function App() {
-
+    const location = useLocation();
+    const hideHeader = location.pathname === "/login";
   return (
     <div style={{minHeight: '100vh', display:'flex', flexDirection: 'column'}}>
-      <Header/>
-      <main style = {{flex: 1, Xpadding: '1rem'}}>
+        {!hideHeader && <Header/>}
+      <main style = {{flex: 1}}>
         <Outlet/>
       </main>
       <Footer/>
--- website/cindi/src/pages/Login/LoginPage.jsx
+++ website/cindi/src/pages/Login/LoginPage.jsx
@@ -1,16 +1,23 @@
 import {GoogleLogin} from '@react-oauth/google';
 import {jwtDecode} from "jwt-decode";
-
+import styles from "./LoginPage.module.css"
 const LoginPage =()=>{
     return(
         <>
-        <div>
-            <GoogleLogin 
-            onSuccess={(credentialResponse) => {
-                console.log(credentialResponse)
-                console.log(jwtDecode(credentialResponse.credential))
-            }}
-            onError={()=>console.log("error log in")}/>
+        <div className={styles.container}>
+            <img src={"/"} alt={"Logo will be here"} className={styles.cindiLogo}/>
+            <div className={styles.loginHeader}>Sign In To Cindi</div>
+            <div className={styles.loginPanel}>
+                <GoogleLogin
+                    onSuccess={(credentialResponse) => {
+                        console.log(credentialResponse)
+                        console.log(jwtDecode(credentialResponse.credential))
+                    }}
+                    onError={()=>console.log("error log in")}
+                    size={"large"}
+                    shape={"pill"}
+                    />
+            </div>
         </div>
         </>
     )
